0e56560e3d2cbb6e70f1a06f35c3b5077de9467e
Tilneil <tilneil.gary@gmail.com>
2019-01-24 03:47:48 +0000

    Contacts App that allow creation of new, edit, delete and search of contacts. Updated page layout, styling. Can't figure out how to do partial search.

--- contacts/app.rb
+++ contacts/app.rb
@@ -7,15 +7,18 @@ set :database, {adapter: 'sqlite3', database: 'contacts.db'}
 class Contact < ActiveRecord::Base
 end
 
+#go to home page and display "all" contacts we have in the database, with options to create new, update, delete or search contacts.
 get '/' do 
     @contacts = Contact.all
-    erb :post
+    erb :home
 end
 
+#open up the webpage with the form for new contacts, the user will enter information here and click.
 get '/new' do
     erb :new
 end
 
+#the submit button will take the information from the new contact form and save it to the database table with the following columns.
 post '/create' do
     contact = Contact.new 
     contact.first_name = params[:first_name]
@@ -27,15 +30,17 @@ post '/create' do
     contact.state = params[:state]
     contact.zip_code = params[:zip_code]
     contact.save 
-    
+#after the user saves, redirect back to the home page. 
     redirect '/'
 end
 
+#when the user selects edit, search the contact by the id that is being selected and open the edit form.
 get '/edit' do
     @contacts = Contact.find_by(id: params[:id])
     erb :edit
 end
 
+#save the changes the users enter in the edit form to the database. 
 post '/update' do
     contact = Contact.find_by(id: params[:id])
     contact.first_name = params[:first_name]
@@ -47,53 +52,45 @@ post '/update' do
     contact.state = params[:state]
     contact.zip_code = params[:zip_code]
     contact.save 
-    
+#redirect back to the home page that shows all the contacts
      redirect '/'    
 end
 
+#select the contact by their id, delete the contact. 
 get '/delete' do
     contact = Contact.find_by(id: params[:id])
     contact.destroy
     redirect '/'
 end
 
-
-# get '/search' do
-#     contact = Contact.all
-#     contact.first_name = "%#{params[:first_name]}%"
-#     contact.last_name = "%#{params[:last_name]}%"
-#     @contacts = Contact.where("first_name LIKE ? OR last_name LIKE ?", contact.first_name, contact.last_name)    
-#   erb :search
-# end
-
+#when the user selects search from the home page, go to the form 'search'. 
 get '/search' do
     erb :find
 end
 
-
+#when the user submits, assign their entries params[...] to the following variables first_name and last_name.
 post '/results' do
     first_name = params[:first_name_search]
     last_name = params[:last_name_search]
+
+#access the database table contacts. Assign the variable @contacts to any database entries that are "like" the information the user entered for the variables we assigned earlier. 
+    @contacts = Contact.where("first_name LIKE ? OR last_name LIKE ?", first_name , last_name )  
+    erb :results
     
-    # #go to database user, search for this email typed in the form.
-    # contact = Contact.find_by( first_name: '%#{params[:first_name]}%',  last_name: '%#{params[:last_name]}')
-    @contacts = Contact.where("first_name LIKE ? OR last_name LIKE ?", first_name , last_name )    
-
-    # @contacts = Contact.
-    # @contact.first_name = Contact.find_by( first_name: params[:first_name] )
-    # @contact.last_name = COntact.find_by( last_name: params[:last_name])
-
-    #     # user = User.find_by( email: params[:email] )
-
-    # if @contact.first_name == '%#{params[:first_name]}%' || @contact.last_name == '%#{params[:last_name]}%' 
-        erb :results
+    # @contacts = Contact.all
+    
+    # if
+    #     @contacts.first_name == Contact.where(" first_name LIKE ? ", '%first_name%') || @contacts.last_name == Contact.where(" last_name LIKE ? ", '%last_name%')  
+    #     erb :results
+        
     # else
     #     erb :contact_not_found
-    # end 
+
+    # end
+    
+    # "'%first_name%'" users = User.where('age > :lowest_age AND age :highest_age ?',{ lowest_age: 10, highest_age: 20})
+
+
+#when the use clicks submit, it's linked to the results page. Open the results page and show all the entries that matches the search above.
+
 end
-
-# get '/results' do
-
-#     erb :results
-# end
-
